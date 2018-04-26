bc4000fa011b16198b4917ec30dea75a793867e3
kalpitha <kalpitha.nl@gmail.com>
2018-04-26 22:18:42 +0530

    Update admin.py

--- data visualizer/uploads/core/admin.py
+++ data visualizer/uploads/core/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Dashboard
-from .models import Entity
-
-admin.site.register(Dashboard)
-admin.site.register(Entity)
 
 # Register your models here.
